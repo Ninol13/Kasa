e109b6442093a9f7642263a70c626c3bb373d927
Nino <nino.litim@epitech.eu>
2025-08-22 15:03:01 +0200

    edit footer for responsive

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from "../../assets/images/logo/logo_footer.png"
+
 const Footer = () => {
-    return (
-      <footer className="footer">
-        <img src={logo} alt="" className="footer__logo" />
-        <p className="footer__text-copyright">
-          © 2020 Kasa. All rights reserved
-        </p>
-      </footer>
-    );
+  return (
+    <footer className="footer">
+      <img src={logo} alt="" className="footer__logo" />
+      <p className="footer__text-copyright">
+        © 2020 Kasa. All{' '}
+        <span className="footer__mobile-br" />
+        rights reserved
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
